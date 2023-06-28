eb0836d414cb5931d6db94e3ed97a2769a465cff
damian.murphy <damian.murphy@ward.ie>
2023-06-28 14:29:05 +0100

    Renamed to end in .asm to avoid conflicting with .z80 extension which is an emulator snapshot format.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 zmakebas -a 10 -n loader -o loader.tap loader.bas
-pasmo -v --tap hello.z80 bitz.tap
+pasmo -v --tap hello.z80.asm bitz.tap
 cat loader.tap bitz.tap > hello.tap
 rm loader.tap bitz.tap
